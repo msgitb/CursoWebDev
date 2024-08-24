9ac606ab8b8746bf5f24ed6b5c9ffc2c64998d52
Marcelo Silva <omarcelito@gmail.com>
2024-08-24 13:25:55 +0100

    PersonalBlog - add build script

---
+++ PersonalBlog/webpack.config.js
@@ -0,0 +1,15 @@
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "final.js",
+  },
+  target: "node",
+};
